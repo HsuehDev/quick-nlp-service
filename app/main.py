c72235a63a2e1cfcd9e95d0a5f100edcd464773a
bighsueh <ss313021490@gmail.com>
2024-02-12 21:55:23 +0800

    add description

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app.nlp_service.routes import router as nlp
+from app.app_intro import description
 
-app = FastAPI(title = 'LLM Gateway',description = '透過 api 快速調用 openai 服務')
+app = FastAPI(
+    title = 'LLM Gateway',
+    description = description
+    )
 api_router = APIRouter()   
 
 api_router.include_router(nlp) 
