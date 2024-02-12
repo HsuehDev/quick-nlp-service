d18f7f514cf717446d75ce87bc332291864f970f
bighsueh <ss313021490@gmail.com>
2024-02-12 21:55:23 +0800

    add description

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from app.nlp_service.routes import router as nlp
+from app.app_intro import description
 
-app = FastAPI(title = 'Wulab Openai 服務平台',description = '透過 api 快速調用 openai 服務')
+app = FastAPI(
+    title = 'Wulab Openai 服務平台',
+    description = description
+    )
 api_router = APIRouter()   
 
 api_router.include_router(nlp) 
