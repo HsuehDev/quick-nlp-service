85742c6d3a2d1baac5744927e1f652f08e1c4822
bighsueh <ss313021490@gmail.com>
2024-04-09 20:34:57 +0800

    add taiwan llama tgi strategy

--- app/nlp_service/strategies/strategy_registry.py
+++ app/nlp_service/strategies/strategy_registry.py
@@ -2,6 +2,7 @@ from app.nlp_service.strategies.strategy import NLPInterface
 from app.nlp_service.strategies.openai_strategy import OpenAIStrategy
 # from app.nlp_service.strategies.llama2_strategy import LLaMa2Strategy
 from app.nlp_service.strategies.tgi_strategy import TGIStrategy
+from app.nlp_service.strategies.taiwan_tgi_strategy import TaiwanTGIStrategy
 
 nlp_strategies = {
     # "huggingface": self.huggingface_nlp_service,
@@ -12,5 +13,6 @@ nlp_strategies = {
     "gpt-4-32k": OpenAIStrategy(),
     
     'wulab': TGIStrategy(),
+    'taiwan-llama' : TaiwanTGIStrategy()
     # "llama-2": LLaMa2Strategy(),
 }
