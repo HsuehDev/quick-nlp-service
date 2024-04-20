dc13f0e368cda80f815b7fd351f6fc1a8102c182
bighsueh <ss313021490@gmail.com>
2024-04-20 14:58:35 +0800

    add(intro): 1. add taiwan-llama to app_intro.py

--- app/app_intro.py
+++ app/app_intro.py
@@ -32,7 +32,7 @@ Following is the data format for the request body:
 5. gpt-4-32k              | 
 
 6. lab-llm                | From the self-hosted HuggingFace TGI api service.
-                          | The following parameters are not available :
+7. taiwan-llama           | The following parameters are not available :
                           |  1. frequency_penalty
                           |  2. presence_penalty
                           |  3. stop
